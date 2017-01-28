e1b4f8046b3d2ee78bb7240f37e34922225ed2d3
px3l <px3l@tuta.io>
2017-01-28 16:04:02 +0000

    added lite script with no kernel clearing

---
+++ linux-cleanser-lite.sh
@@ -0,0 +1,48 @@
+#This has been adapted from the kali-cleaner by MasterButcher
+#Kudos to him for the difficult bit.
+
+#!/bin/bash
+echo -e
+echo -e
+GRAY="\033[1;30m"
+RED="\033[0;31m"
+ENDCOLOR="\033[0m"
+echo -e $GRAY"  ====================================================  "$ENDCOLOR
+echo -e $GRAY" ===                                                === "$ENDCOLOR
+echo -e $GRAY"==               "$RED"Linux-cleanser Lite by px3l"$GRAY"          =="$ENDCOLOR
+echo -e $GRAY" ===                                                === "$ENDCOLOR
+echo -e $GRAY"  ====================================================  "$ENDCOLOR
+echo -e
+echo -e
+
+YELLOW="\033[1;33m"
+RED="\033[0;31m"
+ENDCOLOR="\033[0m"
+ 
+if [ $USER != root ]; then
+echo -e $RED"[Linux-cleanser]:Error: must be root"
+echo -e $YELLOW"[Linux-cleanser]:Exiting..."$ENDCOLOR
+echo -e
+exit 0
+fi
+
+echo -e $YELLOW"[Linux-cleanser]:Flushing local cache from the retrieved package files..."$ENDCOLOR
+sudo apt-get clean
+
+echo -e $YELLOW"[Linux-cleanser]:Clearing non-necessary packages..."$ENDCOLOR
+sudo apt-get autoclean
+
+echo -e $YELLOW"[Linux-cleanser]:Removing redundant dependencies..."$ENDCOLOR
+sudo apt-get -y autoremove
+
+echo -e $YELLOW"[Linux-cleanser]:Cleaning apt cache..."$ENDCOLOR
+sudo aptitude clean
+
+echo -e $YELLOW"[Linux-cleanser]:Emptying the trash..."$ENDCOLOR
+rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
+rm -rf /root/.local/share/Trash/*/** &> /dev/null
+
+echo -e $YELLOW"[Linux-cleanser]:Script Finished!"$ENDCOLOR
+echo -e
+echo -e $RED"Cleansing complete."$ENDCOLOR
+echo -e
